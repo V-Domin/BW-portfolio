fac1080784f60ff40ec3f58a50c25e88ebed1389
Vitaliy <hdhdhdxhhs@gmail.com>
2023-06-19 13:40:18 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// const navLinks = document.querySelectorAll('.link');
+
+// navLinks.forEach(link => {
+//   link.addEventListener('click', function() {
+//     navLinks.forEach(otherLink => {
+//       if (otherLink !== link) {
+//         otherLink.classList.remove('active');
+//       }
+//     });
+    
+//     link.classList.add('active');
+//   });
+// });
